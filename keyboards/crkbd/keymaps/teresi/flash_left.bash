41ec42264424a5fab2e0e59fcb0741d2150b04af
Michael Teresi <miketeresi@gmail.com>
2022-03-14 22:41:12 -0400

    add teresi keymap and etc

---
+++ keyboards/crkbd/keymaps/teresi/flash_left.bash
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# the pro micro is on the left and uses caterina bootloader (default)
+qmk flash -kb crkbd -km teresi
---
+++ keyboards/crkbd/keymaps/teresi/flash_right.bash
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# the elite C is on the right and needs dfu
+qmk flash -kb crkbd -km teresi -bl dfu
